aa225a8f1c9856db01f8ccb1fa6ce368e72cb377
Daniel Russom <daniel.russom00@gmail.com>
2018-01-06 18:10:04 +0000

    Display Screen
    -Placeholder Screen now displays when run

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,53 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
+	private Stage primaryStage;
+	private AnchorPane rootLayout;
+
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Pirate Game");
+
+		showGameDisplay();
+	}
+
+	/**
+	 * Initializes the root layout.
+	 */
+	public void showGameDisplay() {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/GameDisplay.fxml"));
+			rootLayout = (AnchorPane) loader.load();
+
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	/**
+	 * Returns the main stage.
+	 * 
+	 * @return primaryStage
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
